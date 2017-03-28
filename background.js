b042bd5d3c2fd3e8f73732cb168700ae64475ecd
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2017-03-27 23:26:52 -0400

    Removed unnecessary code from the background script The 'Default View' and 'Theater View' buttons now work properly when you click them in 'Full Browser View'

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-// background.js
-
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
@@ -8,12 +6,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
-
-// This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
--- content.js
+++ content.js
@@ -11,6 +11,78 @@ var video = document.getElementById("player-api");
 //Whether the video is in full browser mode or not
 var fullBrowser = false;
 
+function FullBrowser() {
+	//The video that is on the screen
+    //The controls for the video at the bottom
+	var video2 = document.getElementsByClassName("video-stream html5-main-video")[0];
+	var controls = document.getElementsByClassName("ytp-chrome-bottom")[0];
+
+    //Hide The scrollbars on the website
+    htmlBody.style.overflow = "hidden";
+
+    //Remove the banner off the browser
+    topBar.style.top = "-50px";
+	topBar2.style.height = "0px";
+
+	//Increase the size of the video to fit the entire browser
+	//Move the video to the left side of the browser
+	video.style.position = "fixed";
+	video.style.width = "100vw";
+	video.style.height = "100vh";
+	video.style.top = "0px";
+	video.style.left = "0px";
+	video.style.marginLeft = "0px";
+
+	//Change the resolution of the video to fit the browser
+	video2.style.width = "100vw";
+	video2.style.height = "100vh";
+
+	//Change the size of the controls to fit the browser
+	//Remove the offset
+	controls.style.width = "98%";
+	controls.style.left = "1%";
+
+	//Set that the video is in full browser mode
+	fullBrowser = true;
+}
+
+function OriginalBrowser() {
+
+	//The video that is on the screen
+    //The controls for the video at the bottom
+	var video2 = document.getElementsByClassName("video-stream html5-main-video")[0];
+    var controls = document.getElementsByClassName("ytp-chrome-bottom")[0];
+
+    //Make the scroll bars visible again
+    htmlBody.style.overflow = null;
+
+    //Move the top banner back into the browser
+    topBar.style.top = null;
+	topBar2.style.height = null;
+
+	//Reset the size of the video
+	//Center the video
+	video.style.width = null;
+	video.style.height = null;
+	video.style.top = null;
+	video.style.left = null;
+	video.style.marginLeft = null;
+	video.style.position = null;
+
+	//Set the video back to the default dimensions
+	//Remove the offset of the video
+	video2.style.width = "100%";
+	video2.style.height = "100%";
+	video2.style.left = "0px";
+
+	//Set the width of the controls to the size of the video
+	controls.style.width = "98%";
+	controls.style.left = "1%";
+
+	//Set that the video isn't in full browser mode
+	fullBrowser = false;
+}
+
 //Waiting for the extension button to be clicked
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -20,73 +92,16 @@ chrome.runtime.onMessage.addListener(
       if (fullBrowser == false &&
       window.location.href.includes("watch")) {
 
-      	//The video that is on the screen
-      	//The controls for the video at the bottom
-		var video2 = document.getElementsByClassName("video-stream html5-main-video")[0];
-		var controls = document.getElementsByClassName("ytp-chrome-bottom")[0];
-
-      	//Hide The scrollbars on the website
-      	htmlBody.style.overflow = "hidden";
-
-      	//Remove the banner off the browser
-      	topBar.style.top = "-50px";
-		topBar2.style.height = "0px";
-
-		//Increase the size of the video to fit the entire browser
-		//Move the video to the left side of the browser
-		video.style.position = "fixed";
-		video.style.width = "100vw";
-		video.style.height = "100vh";
-		video.style.top = "0px";
-		video.style.left = "0px";
-		video.style.marginLeft = "0px";
-
-		//Change the resolution of the video to fit the browser
-		video2.style.width = "100vw";
-		video2.style.height = "100vh";
-
-		//Change the size of the controls to fit the browser
-		//Remove the offset
-		controls.style.width = "98%";
-		controls.style.left = "1%";
-
-		//Set that the video is in full browser mode
-		fullBrowser = true;
+      	//Set the video to display on the full browser
+      	//Add a listener to the video mode and run the original size when it's clicked
+      	FullBrowser();
+        document.getElementsByClassName("ytp-size-button ytp-button")[0].addEventListener("click", OriginalBrowser);
 
 	  //Test if the video is in full browser mode
       } else {
 
-      	var video2 = document.getElementsByClassName("video-stream html5-main-video")[0];
-      	var controls = document.getElementsByClassName("ytp-chrome-bottom")[0];
-
-      	//Make the scroll bars visible again
-      	htmlBody.style.overflow = null;
-
-      	//Move the top banner back into the browser
-      	topBar.style.top = null;
-		topBar2.style.height = null;
-
-		//Reset the size of the video
-		//Center the video
-		video.style.width = null;
-		video.style.height = null;
-		video.style.top = null;
-		video.style.left = null;
-		video.style.marginLeft = null;
-		video.style.position = null;
-
-		//Set the video back to the default dimensions
-		//Remove the offset of the video
-		video2.style.width = "100%";
-		video2.style.height = "100%";
-		video2.style.left = "0px";
-
-		//Set the width of the controls to the size of the video
-		controls.style.width = "98%";
-		controls.style.left = "1%";
-
-		//Set that the video isn't in full browser mode
-		fullBrowser = false;
+      	//Return the video back to the original size
+      	OriginalBrowser();
       }
     }
   }
